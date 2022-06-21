8626efb7beaf4e706ff19bde7af35e69be620256
yatorho <3227068950@qqcom>
2022-06-21 16:31:20 +0800

    optional container using synatex c++11

---
+++ chime/core/util/optional.hpp
@@ -0,0 +1,143 @@
+// Copyright by 2022.6 chime
+// author: yatorho
+
+#ifndef CHIME_CORE_UTIL_OPTIONAL_HPP_
+#define CHIME_CORE_UTIL_OPTIONAL_HPP_
+
+#include <type_traits>
+#include <utility>
+
+#include "chime/core/framework/common.hpp"
+
+namespace chime {
+
+template <typename Tp>
+class Optional {
+  using data_type =
+      typename std::aligned_storage<sizeof(Tp),
+                                    std::alignment_of<Tp>::value>::type;
+  using type_ptr = Tp *;
+
+ public:
+  Optional() : _inited(false) {}
+
+  Optional(const Tp &value) { create(value); }
+
+  Optional(Tp &&value) { create(std::move(value)); }
+
+  ~Optional() { destroy(); }
+
+  Optional(const Optional &other) : _inited(false) {
+    if (other._inited) assign(other);
+  }
+
+  Optional(Optional &&other) : _inited(false) {
+    if (other._inited) {
+      assign(std::move(other));
+    }
+  }
+
+  Optional &operator=(const Optional &other) {
+    assign(other);
+    return &this;
+  }
+
+  Optional &operator=(Optional &&other) {
+    assign(std::move(other));
+    return *this;
+  }
+
+  template <class... Args>
+  void emplace(Args &&...args) {
+    destroy();
+    create(std::forward<Args>(args)...);
+  }
+
+  bool is_init() const { return _inited; }
+
+  explicit operator bool() const { return is_init(); }
+
+  Tp &operator*() { return *reinterpret_cast<type_ptr>(&_data); }
+
+  const Tp &operator*() const {
+    return *reinterpret_cast<const type_ptr>(&_data);
+  }
+
+  Tp *operator->() {
+    return is_init() ? reinterpret_cast<type_ptr>(&_data) : nullptr;
+  }
+
+  const Tp *operator->() const {
+    return is_init() ? reinterpret_cast<const type_ptr>(&_data) : nullptr;
+  }
+
+  Tp &value() {
+    if (is_init()) return **this;
+    LOG(FATAL) << "Optional container have not been initialized.";
+  }
+
+  const Tp &value() const {
+    if (is_init()) return **this;
+    LOG(FATAL) << "Optional container have not been initialized.";
+  }
+
+  bool operator==(const Optional<Tp> &rhs) const {
+    return (!bool(*this)) != (!rhs)
+               ? false
+               : (!bool(*this) ? true : (*(*this)) == (*rhs));
+  }
+
+  bool operator<(const Optional<Tp> &rhs) const {
+    return !rhs ? false : (!bool(*this) ? true : (*(*this) < (*rhs)));
+  }
+
+  bool operator!=(const Optional<Tp> &rhs) { return !(*this == (rhs)); }
+
+ private:
+  template <class... Args>
+  void create(Args &&...args) {
+    new (&_data) Tp(std::forward<Args>(args)...);
+    _inited = true;
+  }
+
+  void destroy() {
+    if (_inited) {
+      _inited = false;
+      ((type_ptr)(&_data))->~Tp();
+    }
+  }
+
+  void assign(const Optional &other) {
+    if (other._inited) {
+      _copy(other._data);
+      _inited = true;
+    } else
+      destroy();
+  }
+
+  void assign(Optional &&other) {
+    if (other._inited) {
+      _move(std::move(other._data));
+      _inited = true;
+      other.destroy();
+    } else
+      destroy();
+  }
+
+  void _move(data_type &&val) {
+    destroy();
+    new (&_data) Tp(std::move(*(type_ptr)(&val)));
+  }
+
+  void _copy(const data_type &val) {
+    destroy();
+    new (&_data) Tp(*(type_ptr)(&val));
+  }
+
+ private:
+  bool _inited;
+  data_type _data;
+};
+}  // namespace chime
+
+#endif  // CHIME_CORE_UTIL_OPTIONAL_HPP_
---
+++ chime/core/util/optional_test.cc
@@ -0,0 +1,56 @@
+// Copyright by 2022.6 chime
+// author: yatorho
+
+#include "chime/core/util/optional.hpp"
+
+#include <optional>
+
+#include "chime/core/framework/common.hpp"
+
+namespace chime {
+
+struct Object {
+  int x;
+  int y;
+  int z;
+};
+
+TEST(Optional, TestOperatorBool) {
+  Optional<int> opt_int;
+  EXPECT_FALSE(opt_int);
+  opt_int = 2;
+  EXPECT_TRUE(opt_int);
+}
+
+TEST(Optional, TestDereference) {
+  Optional<Object> opt;
+  EXPECT_FALSE(opt);
+  Object obj = {3, 4, 5};
+  opt = obj;
+  EXPECT_TRUE(opt);
+  (*opt).x = 6;
+  EXPECT_EQ(opt.value().x, 6);
+  EXPECT_EQ(opt->x, 6);
+}
+
+TEST(Optional, TestMoveConstructor) {
+  Optional<Object> opt(Object({2, 3, 4}));
+  EXPECT_EQ(opt->x, 2);
+  opt = Object({3, 4, 5});
+  EXPECT_EQ(opt->x, 3);
+  Object obj = Object({4, 5, 6});
+  opt = std::move(obj);
+  EXPECT_EQ(opt->x, 4);
+}
+
+TEST(Optioanl, TestOperatorAssign) {
+  Optional<Object> opt1(Object({2, 3, 4}));
+
+  auto opt2 = opt1;
+  EXPECT_EQ(opt2->x, 2);
+  opt1->x = 3;
+  opt2 = std::move(opt1);
+  EXPECT_EQ(opt2->x, 3);
+}
+
+}  // namespace chime
