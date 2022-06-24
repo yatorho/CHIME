30e0ca9fd5d17dfbd408b77dbd3d31de4cfa6354
yatorho <3227068950@qqcom>
2022-06-24 13:29:21 +0800

    delete queue series files in platform, which were added in util parts

--- chime/core/util/ring_queue.hpp
+++ chime/core/util/ring_queue.hpp
@@ -4,18 +4,20 @@
 #ifndef CHIME_CORE_UTIL_RING_QUEUE_HPP_
 #define CHIME_CORE_UTIL_RING_QUEUE_HPP_
 
-#include <atomic>
 #include <cstdint>
+#include <mutex>
+#include <queue>
 
 #include "chime/core/util/container_wrapper.hpp"
-#include "chime/core/util/fixed_queue.hpp"
 
 namespace chime {
 namespace concurrent {
 
+using util::ContainerWrapper;
+
 template <typename Tp, bool strict = false,
           typename Container =
-              typename ContainerWrapper<Tp, FixedQueue<Tp>>::container_type>
+              typename ContainerWrapper<Tp, std::queue<Tp>>::container_type>
 class RingQueue {
   typedef Container container_type;
   typedef typename ContainerWrapper<Tp, container_type>::size_type size_type;
@@ -25,6 +27,8 @@ class RingQueue {
   typedef typename ContainerWrapper<Tp, container_type>::pointer pointer;
   typedef typename ContainerWrapper<Tp, container_type>::const_pointer
       const_pointer;
+  typedef std::mutex mutex_type;
+  typedef std::unique_lock<std::mutex> unique_lock;
 
  public:
   RingQueue(size_type size) : _capacity(size) {}
@@ -33,20 +37,25 @@ class RingQueue {
 
   bool full() const { return _queue.size() == _capacity; }
 
+  size_type size() const { return _queue.size(); }
+
   size_type capacity() const { return _capacity; }
 
   bool add_element(const Tp &element) {
+    unique_lock lock(_mutex);
     return full() ? false : (_queue.push(element), true);
   }
 
   bool get_element(Tp *ptr) {
-    return empty() ? false : (ptr = &_queue.front(), _queue.pop(), true);
+    unique_lock lock(_mutex);
+    return empty() ? false : (*ptr = _queue.front(), _queue.pop(), true);
   }
 
  private:
   uint64_t _capacity;
 
   container_type _queue;
+  mutex_type _mutex;
 
   /// Disabled copy and assignment
   RingQueue(const RingQueue &);
--- chime/core/util/ring_queue_test.cc
+++ chime/core/util/ring_queue_test.cc
@@ -6,6 +6,7 @@
 #include <thread>
 
 #include "chime/core/framework/common.hpp"
+#include "chime/core/platform/env.hpp"
 
 namespace chime {
 namespace concurrent {
@@ -16,104 +17,110 @@ struct Object {
 };
 
 TEST(RingQueue, TestConstructorandIsEmpty) {
-  RingQueue<int> ring_buffer(4);
-  EXPECT_FALSE(ring_buffer.full());
-  EXPECT_TRUE(ring_buffer.empty());
-  EXPECT_EQ(ring_buffer.capacity(), 4);
+  RingQueue<int> ring_queue(4);
+  EXPECT_FALSE(ring_queue.full());
+  EXPECT_TRUE(ring_queue.empty());
+  EXPECT_EQ(ring_queue.capacity(), 4);
 }
 
-TEST(RingQueue, TestIsFull) {
-  RingQueue<int *> ring_buffer(10);
-  auto *data = new int[11];
-  for (int32_t i = 0; i < 10; i++) {
-    data[i] = i;
-    EXPECT_TRUE(ring_buffer.add_element(&data[i]));
-    if (i != 9) {
-      EXPECT_FALSE(ring_buffer.full());
-    }
-  }
-  EXPECT_TRUE(ring_buffer.full());
-  data[10] = 10;
-  EXPECT_FALSE(ring_buffer.add_element(&data[10]));
+TEST(RingQueue, TestFull) {
+  RingQueue<int> ring_queue(4);
+  EXPECT_FALSE(ring_queue.full());
+  EXPECT_TRUE(ring_queue.empty());
+  EXPECT_EQ(ring_queue.capacity(), 4);
+  ring_queue.add_element(1);
+  EXPECT_FALSE(ring_queue.full());
+  EXPECT_FALSE(ring_queue.empty());
+  ring_queue.add_element(2);
+  ring_queue.add_element(3);
+  ring_queue.add_element(4);
+  EXPECT_TRUE(ring_queue.full());
+  EXPECT_FALSE(ring_queue.empty());
+  EXPECT_EQ(ring_queue.capacity(), 4);
+  EXPECT_FALSE(ring_queue.add_element(5));
 }
 
 TEST(RingQueue, TestIsFullMultiThread) {
   const int count = 500;
   auto *data = new Object[count];
-  RingQueue<Object *> ring_buffer(count);
+  RingQueue<Object *> ring_queue(count);
 
   std::atomic_uint over_count;
   over_count = 0;
-  std::thread thread1([&data, &ring_buffer, &over_count, &count]() {
+  std::thread thread1([&data, &ring_queue, &over_count, &count]() {
     for (int32_t i = 0; i < count; i++) {
-      if (!ring_buffer.add_element(&data[i])) ++over_count;
+      if (!ring_queue.add_element(&data[i])) ++over_count;
     }
   });
-  std::thread thread2([&data, &ring_buffer, &over_count, &count]() {
+  std::thread thread2([&data, &ring_queue, &over_count, &count]() {
     for (int32_t i = 0; i < count; i++) {
-      if (!ring_buffer.add_element(&data[i])) ++over_count;
+      if (!ring_queue.add_element(&data[i])) ++over_count;
     }
   });
-  std::thread thread3([&data, &ring_buffer, &over_count, &count]() {
+  std::thread thread3([&data, &ring_queue, &over_count, &count]() {
     for (int32_t i = 0; i < count; i++) {
-      if (!ring_buffer.add_element(&data[i])) ++over_count;
+      if (!ring_queue.add_element(&data[i])) ++over_count;
     }
   });
 
   thread1.join();
   thread2.join();
   thread3.join();
+  EXPECT_TRUE(ring_queue.full());
   EXPECT_EQ(over_count, 2 * count);
 }
 
-// TEST(RingQueue, TestConsume) {
-//   const int count = 500;
-//   auto *data = new Object[count];
-//   Object *d_ptr = nullptr;
-//   RingQueue<Object> ring_buffer(count / 2);
+TEST(RingQueue, TestConsume) {
+  const int count = 500;
+  auto *data = new Object[count];
+  Object *d_ptr = nullptr;
+  RingQueue<Object *> ring_queue(count / 2);
 
-//   std::atomic_uint over_count;
-//   over_count = 0;
+  std::atomic_uint over_count;
+  over_count = 0;
 
-//   std::thread thread1([&data, &ring_buffer, &over_count, &count]() {
-//     for (int32_t i = 0; i < count; i++) {
-//       while (!ring_buffer.add_element(&data[i]))
-//         ;
-//     }
-//   });
+  std::thread thread1([&data, &ring_queue, &over_count, &count]() {
+    for (int32_t i = 0; i < count; i++) {
+      while (!ring_queue.add_element(&data[i]))
+        ;
+    }
+  });
 
-//   std::thread thread2([&data, &ring_buffer, &over_count, &count, &d_ptr]() {
-//     for (int32_t i = 0; i < count / 2; i++) {
-//       while (!ring_buffer.get_element(&d_ptr))
-//         ;
-//     }
-//   });
-//   thread1.join();
-//   thread2.join();
+  std::thread thread2([&data, &ring_queue, &over_count, &count, &d_ptr]() {
+    for (int32_t i = 0; i < count / 2; i++) {
+      while (!ring_queue.get_element(&d_ptr))
+        ;
+    }
+  });
+  thread1.join();
+  thread2.join();
 
-//   EXPECT_TRUE(ring_buffer.full());
-// }
+  EXPECT_TRUE(ring_queue.full());
+}
 
-// // Test Thread* enqueue RingQueue and dequeue RingQueue
-// TEST(RingQueue, TestEnvThreadQueue) {
-//   std::atomic<int> value;
-//   value = 0;
+// Test Thread* enqueue RingQueue and dequeue RingQueue
+TEST(RingQueue, TestEnvThreadQueue) {
+  using platform::Env;
+  using platform::Thread;
+  using platform::ThreadOptions;
+  std::atomic<int> value;
+  value = 0;
 
-//   Env *env = Env::Default();
+  Env *env = Env::Default();
 
-//   RingQueue<Thread> ring(10);
-//   for (int i = 0; i < 10; ++i) {
-//     ring.add_element(
-//         env->start_thread(ThreadOptions(), "test", [&value]() { value++; }));
-//   }
+  RingQueue<Thread *> ring(10);
+  for (int i = 0; i < 10; ++i) {
+    ring.add_element(
+        env->start_thread(ThreadOptions(), "test", [&value]() { value++; }));
+  }
 
-//   Thread *thread;
+  Thread *thread;
 
-//   for (int i = 0; i < 10; ++i) {
-//     ring.get_element(&thread);
-//     thread->~Thread();
-//   }
-//   EXPECT_EQ(value, 10);
-// }
+  for (int i = 0; i < 10; ++i) {
+    ring.get_element(&thread);
+    thread->~Thread();
+  }
+  EXPECT_EQ(value, 10);
+}
 }  // namespace concurrent
 }  // namespace chime
